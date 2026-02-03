8d3bd53fb8fb5f6d0130327ac47f33881e1b7df3
Tasnim Ahmed <tasnim@tazzix.com>
2026-02-03 21:40:59 +0500

    Delete client directory

--- client/src/types/index.ts
+++
@@ -1,8 +0,0 @@
-export interface GameState {
-  board: (string | null)[];
-  currentPlayer: 'X' | 'O';
-  winner: string | null;
-  isDraw: boolean;
-  mode: 'human' | 'computer';
-  gameId?: string;
-}
